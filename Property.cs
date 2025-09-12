761dba73d4a232a94150f6feaa066b8f0c9ea743
wominicdallace-ui <c3354956@uon.edu.au>
2025-09-12 23:42:44 +1000

    Update Property.cs

--- Property.cs
+++ Property.cs
@@ -1,7 +1,12 @@
+using SQLite;
+
 namespace Rentrey;
 
 public class Property
 {
+    [PrimaryKey, AutoIncrement]
+    public int Id { get; set; }
+
     public string ImageSource { get; set; }
     public string Details { get; set; }
     public string Address { get; set; }
