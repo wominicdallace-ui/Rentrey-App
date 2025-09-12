2facc8115aeeb7df59fb1434093886337796a5a6
wominicdallace-ui <c3354956@uon.edu.au>
2025-09-12 23:40:04 +1000

    Update ApplicationPage.xaml.cs

--- ApplicationPage.xaml.cs
+++ ApplicationPage.xaml.cs
@@ -1,9 +1,50 @@
-namespace Rentrey.Maui;
+using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Globalization;
+using System;
+using RentreyApp.Services;
 
-public partial class ApplicationPage : ContentPage
+namespace Rentrey.Maui
 {
-	public ApplicationPage()
-	{
-		InitializeComponent();
-	}
+    public partial class ApplicationPage : ContentPage, INotifyPropertyChanged
+    {
+        private readonly DatabaseService _databaseService;
+
+        private ObservableCollection<ApplicationItem> _applications;
+        public ObservableCollection<ApplicationItem> Applications
+        {
+            get => _applications;
+            set
+            {
+                if (_applications != value)
+                {
+                    _applications = value;
+                    OnPropertyChanged(nameof(Applications));
+                }
+            }
+        }
+
+        public ApplicationPage(DatabaseService databaseService)
+        {
+            InitializeComponent();
+            _databaseService = databaseService;
+
+            LoadApplications();
+
+            BindingContext = this;
+        }
+
+        private async void LoadApplications()
+        {
+            var applications = await _databaseService.GetApplicationsAsync();
+            Applications = new ObservableCollection<ApplicationItem>(applications);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
 }
