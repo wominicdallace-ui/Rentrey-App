a7e5e13f59a8ca26639f644e37ef98a40b7eefaa
wominicdallace-ui <c3354956@uon.edu.au>
2025-09-12 18:38:20 +1000

    Update AccountPage.xaml.cs

--- AccountPage.xaml.cs
+++ AccountPage.xaml.cs
@@ -1,7 +1,12 @@
 using Microsoft.Maui.Controls;
+using Microsoft.Maui.Graphics;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Globalization;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Storage;
 
 namespace Rentrey.Maui
 {
@@ -38,8 +43,22 @@ namespace Rentrey.Maui
     }
 
     // The ViewModel for the page, implementing INotifyPropertyChanged
-    public partial class AccountPage : ContentPage
+    public partial class AccountPage : ContentPage, INotifyPropertyChanged
     {
+        private string _profileImageSource;
+        public string ProfileImageSource
+        {
+            get => _profileImageSource;
+            set
+            {
+                if (_profileImageSource != value)
+                {
+                    _profileImageSource = value;
+                    OnPropertyChanged(nameof(ProfileImageSource));
+                }
+            }
+        }
+
         public string UserName { get; set; }
         public string Points { get; set; }
         public string LastUpdated { get; set; }
@@ -77,7 +96,84 @@ namespace Rentrey.Maui
                 new Badge { IconSource = "badge_new.png", Title = "New User", Description = "Joined the Rentrey community" }
             };
 
+            // Set a default profile image
+            ProfileImageSource = "profilepicture.png";
+
             this.BindingContext = this;
+        }
+
+        private async Task<PermissionStatus> GetPermissionAsync<T>() where T : Permissions.BasePermission, new()
+        {
+            PermissionStatus status = await Permissions.CheckStatusAsync<T>();
+
+            if (status != PermissionStatus.Granted)
+            {
+                status = await Permissions.RequestAsync<T>();
+            }
+
+            return status;
+        }
+
+        private async void LoadImage(object sender, EventArgs e)
+        {
+            FileResult photo = null;
+
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                string action = await DisplayActionSheet("Select Image Source", "Cancel", null, "Take Photo", "Choose from Gallery");
+
+                if (action == "Take Photo")
+                {
+                    PermissionStatus status = await GetPermissionAsync<Permissions.Camera>();
+                    if (status == PermissionStatus.Granted)
+                    {
+                        photo = await MediaPicker.CapturePhotoAsync();
+                    }
+                    else
+                    {
+                        await DisplayAlert("Permission Denied", "Camera permission is required to take photos.", "OK");
+                        return;
+                    }
+                }
+                else if (action == "Choose from Gallery")
+                {
+                    PermissionStatus status = await GetPermissionAsync<Permissions.Photos>();
+                    if (status == PermissionStatus.Granted)
+                    {
+                        photo = await MediaPicker.PickPhotoAsync();
+                    }
+                    else
+                    {
+                        await DisplayAlert("Permission Denied", "Photo library permission is required to pick photos.", "OK");
+                        return;
+                    }
+                }
+            }
+            else
+            {
+                // Fallback for devices without camera support
+                photo = await MediaPicker.PickPhotoAsync();
+            }
+
+            if (photo != null)
+            {
+                string imagesDir = System.IO.Path.Combine(FileSystem.Current.AppDataDirectory, "images");
+                System.IO.Directory.CreateDirectory(imagesDir);
+
+                var newFile = System.IO.Path.Combine(imagesDir, photo.FileName);
+                using (var stream = await photo.OpenReadAsync())
+                using (var newStream = File.OpenWrite(newFile))
+                {
+                    await stream.CopyToAsync(newStream);
+                }
+                ProfileImageSource = newFile;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
