51d6ce028623820c45fc17ea39862759597e25b0
wominicdallace-ui <c3354956@uon.edu.au>
2025-09-12 23:08:23 +1000

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,15 +1,12 @@
-﻿namespace Rentrey
+namespace Rentrey
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-        }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
+            MainPage = new AppShell();
         }
     }
 }
