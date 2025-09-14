f9f75c51f08b790a85ae2eefcd2c74766fe6298c
wominicdallace-ui <c3354956@uon.edu.au>
2025-09-14 19:21:02 +1000

    Update LoginPage.xaml.cs

--- LoginPage.xaml.cs
+++ LoginPage.xaml.cs
@@ -1,20 +1,46 @@
 using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 
 namespace Rentrey.Maui
 {
-    public partial class LoginPage : ContentPage
+    public partial class LoginPage : ContentPage, INotifyPropertyChanged
     {
+        public ObservableCollection<string> StatesList { get; set; }
+        public string State { get; set; }
+
         public LoginPage()
         {
             InitializeComponent();
+
+            StatesList = new ObservableCollection<string>
+            {
+                "New South Wales",
+                "Victoria",
+                "Queensland",
+                "South Australia",
+                "Western Australia",
+                "Tasmania",
+                "Australian Capital Territory",
+                "Northern Territory"
+            };
+
+            this.BindingContext = this;
         }
 
         private async void OnCreateAccountClicked(object sender, EventArgs e)
         {
+            // You can now access the selected state using this.State
             string userName = FirstNameEntry.Text;
 
-            // Navigate to the HomePage tab
+            // This is the correct way to navigate from the login page to the main app page.
             await Shell.Current.GoToAsync("//HomePage");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
