b288ebc6055db00bca16b2b2b75314dd72f9e726
wominicdallace-ui <c3354956@uon.edu.au>
2025-09-23 18:04:32 +1000

    Update PropertyPage.xaml.cs

--- PropertyPage.xaml.cs
+++ PropertyPage.xaml.cs
@@ -3,11 +3,14 @@ using System.ComponentModel;
 using System.Globalization;
 using System.Collections.Generic;
 using System.Diagnostics;
+using RentreyApp.Services;
+using System.Threading.Tasks;
 
 namespace Rentrey.Maui
 {
-    public partial class PropertyPage : ContentPage, IQueryAttributable
+    public partial class PropertyPage : ContentPage, IQueryAttributable, INotifyPropertyChanged
     {
+        private readonly ProptrackService _proptrackService;
         private Property _property;
 
         public Property Property
@@ -20,17 +23,42 @@ namespace Rentrey.Maui
             }
         }
 
-        public PropertyPage()
+        // Inject the ProptrackService into the constructor
+        public PropertyPage(ProptrackService proptrackService)
         {
             InitializeComponent();
+            _proptrackService = proptrackService;
+            BindingContext = this;
         }
 
-        public void ApplyQueryAttributes(IDictionary<string, object> query)
+        public async void ApplyQueryAttributes(IDictionary<string, object> query)
         {
             if (query.ContainsKey("property") && query["property"] is Property receivedProperty)
             {
                 this.Property = receivedProperty;
-                this.BindingContext = this.Property;
+                
+                // Call the API to get more detailed information using the property's ID
+                await GetPropertyDetailsFromApi(this.Property.Id.ToString());
+            }
+        }
+        
+        // New method to fetch property details from the API
+        private async Task GetPropertyDetailsFromApi(string propertyId)
+        {
+            try
+            {
+                // Call the new GetListingAsync method in your service
+                var detailedProperty = await _proptrackService.GetListingAsync(propertyId);
+                
+                // Update the Property object with the new, detailed information
+                if (detailedProperty != null)
+                {
+                    this.Property = detailedProperty;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Failed to get property details from API: {ex.Message}");
             }
         }
 
